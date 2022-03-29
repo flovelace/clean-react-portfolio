07de5ad54cfa985e20f68543137501ade031887f
fairfax <lovelace.af@gmail.com>
2022-03-28 23:44:01 -0400

    added dataset to data.js

---
+++ components/Hero.js
@@ -0,0 +1,3 @@
+import Image from "next/image";
+import { hero } from '@/data/config'
+import { useTheme } from "next-themes";
---
+++ data/config.js
@@ -0,0 +1,54 @@
+export const meta = {
+    title: 'Chris Lightfellow - Full Stack Web Developer',
+    description: 'Hey, I\'m Chris. I\'m a full stack developer, and a retired knight.'
+};
+
+export const hero = {
+    title: "Greetings! My name is Chris.",
+    desc: "I am a full stack developer. I used to be a knight. Isn't that cool?"
+};
+
+export const projects = {
+    title: 'Chris\'s Cool Projects',
+    desc: "Would you like to see some of my projects?",
+    projects: [
+        {
+            title: 'Fake Website',
+            description: 'A fake website I made',
+            link: 'https://',
+            github: ''
+        },
+        {
+            title: 'Fake App',
+            description: 'A fake app',
+            link: 'https://',
+            github: ''
+        },
+        {
+            title: 'Fake App 2',
+            description: 'A 2nd fake app',
+            link: 'https://',
+            github: ''
+        }
+    ]
+};
+
+export const stack = {
+    title: 'Chris Dev Stack',
+    stack: [
+        'HTML5',
+        'CSS3',
+        'JavaScript',
+        'React',
+        'NextJS',
+        'MongoDB',
+        'SQL',
+    ]
+};
+
+export const contact = {
+    title: 'Contact Chris',
+    email: 'chris@unknightly.com',
+    github: 'chrisknighteternal',
+    linkedin: 'christine-a-lightfellow'
+};
