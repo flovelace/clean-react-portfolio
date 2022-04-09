9c12ec17b83044474606c15ae772149ec5286055
fairfax <lovelace.af@gmail.com>
2022-04-08 23:19:30 -0400

    completed the email section of contact section

--- components/Contact.js
+++ components/Contact.js
@@ -1,2 +1,23 @@
 import Image from 'next/image';
 import { contact } from '@/data/config';
+
+export default function Stack() {
+    return (
+        <div>
+            <div className='overflow-x-hidden w-full'>
+                <h2 className='landingSectionTitle max-w-max mx-0 text-left relative mb-4 md:w-max'>
+                    {contact.title}
+                </h2>
+            </div>
+            <p>
+                Send Chris an email:{' '}
+                <a
+                    className='dark:text-white text-black transition-colors duration-500'
+                    href={`mailto:${contact.email}`}
+                >
+                    {contact.email}
+                </a>
+            </p>
+        </div>
+    )
+}
